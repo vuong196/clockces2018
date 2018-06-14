f166c79e25de4838c0d564e50d16f75dcdf56cdd
Vuong Nguyen <thanhvuongvuong96@gmail.com>
2018-06-15 00:00:22 +0700

    [Sprint-2] Edit index.html, App.vue and main.js for running dev

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <div id="app">
-    <div v-bind:title="randNum" v-if="shown">
-      Hover over me for your random number
+    <getIP></getIP>
+    <div class="container">
+      <search-form></search-form>
     </div>
-  <a class="waves-effect waves-light btn" v-on:click="reveal">button</a>
+    <timezone-form></timezone-form>
   </div>
 </template>
 
 <script>
+// import axios from 'axios'
+import getIp from './components/getIP'
+import searchform from './components/Search'
+import timezoneform from './components/TimezoneForm'
 export default {
   name: 'App',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      randNum: Math.floor(Math.random() * 50) + 1,
-      shown: true
-    }
+  components: {
+    'search-form': searchform,
+    'getIP': getIp,
+    'timezone-form': timezoneform
   },
-  methods: {
-    reveal () {
-      alert(`Here's the secret number: ${this.randNum}`)
-    }
+  data () {
+    return {}
+  },
+  mounted () {
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Board from './Board'
-import Search from './Search'
-import timerow24h from './timerow24h'
-import timezoneinfor from './timezoneinfor'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -14,34 +11,5 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>',
-  render: h => h(App)
-})
-
-new Vue({
-  el: '#board',
-  router,
-  components: { Board },
-  template: '<Board/>'
-})
-
-new Vue({
-  el: '#searchform',
-  router,
-  components: { Search },
-  template: '<Search/>'
-})
-
-new Vue({
-  el: '#timerow24h',
-  router,
-  components: { timerow24h },
-  template: '<timerow24h/>'
-})
-
-new Vue({
-  el: '#timezoneinfor',
-  router,
-  components: { timezoneinfor },
-  template: '<timezoneinfor/>'
+  template: '<App/>'
 })
