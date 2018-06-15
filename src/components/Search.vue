9b1b4efff7fe91cbdeab4ce8c14350b9bd5cbdda
Vuong Nguyen <thanhvuongvuong96@gmail.com>
2018-06-15 13:55:19 +0700

    [Sprint-2] Update Css for Search form

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="searchform">
-        <input type="text" v-model="searchtext" list="listofCities" placeholder="Search city..." />
+      <form>
+        <input type="search" v-model="searchtext" list="listofCities" placeholder="Search city..." />
         <datalist id="listofCities" >
           <option v-for="(city, idx) in cities" :key="idx">
-            <h1>{{city.city}}</h1>
-            <p>{{city.country}}</p>
+            <h1>{{city.id}}</h1>
+            <p>{{city.name}}</p>
           </option>
         </datalist>
+      </form>
   </div>
 </template>
 <script>
@@ -19,15 +21,18 @@ export default {
       searchtext: ''
     }
   },
+  props: ['searchtext'],
   mounted () {
     axios({
       method: 'GET',
-      url: 'https://cors-anywhere.herokuapp.com/https://search-city.herokuapp.com/city'
+      url: 'https://cors-anywhere.herokuapp.com/https://clock-ces.herokuapp.com/topic'
     }).then(
       results => { this.cities = results.data },
       error => {
         console.error(error)
       })
+    
+    
   },
   watch: {
     searchtext: function () {
@@ -35,16 +40,59 @@ export default {
       this.cities = this.cities.filter(city => (city.city.toLowerCase().indexOf(this.searchtext.toLowerCase()) > -1))
     }
   }
-
+  
 }
 </script>
 <style>
 #searchform {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  float:left;
+  margin-top: 6px;
+  margin-left:6px;
+}
+
+#searchform input {
+  outline: none;
+
+}
+
+#searchform input[type=search] {
+
+	font-family: inherit;
+	font-size: 100%;
+  height:50px;
+}
+#searchform input::-webkit-search-decoration,
+input::-webkit-search-cancel-button {
+	display: none;
+}
+
+#searchform input[type=search] {
+	background: #ededed url(https://static.tumblr.com/ftv85bp/MIXmud4tx/search-icon.png) no-repeat 9px center;
+	border: solid 2px #ccc;
+	padding: 9px 10px 9px 32px;
+	width: 150px;
+
+	-webkit-border-radius: 10em;
+	-moz-border-radius: 10em;
+	border-radius: 10em;
+
+	-webkit-transition: all .5s;
+	-moz-transition: all .5s;
+	transition: all .5s;
+}
+#searchform input[type=search]:focus {
+	width: 200px;
+	background-color: #fff;
+	border: solid 2px #a7a7a7;
+
+}
+
+#searchform input:-moz-placeholder {
+	color: #999;
+}
+#searchform input::-webkit-input-placeholder {
+	color: #999;
 }
 </style>
