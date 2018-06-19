76e2b69c402363f6723e867606397aeb9b4ed92b
Vuong Nguyen <thanhvuongvuong96@gmail.com>
2018-06-19 13:52:04 +0700

    Edit server.js for deployment

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started ' + port);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+// create the express app
+const app = express()
+// create middleware to handle the serving the app
+app.use("/", serveStatic(path.join(__dirname, '/dist')))
+// Create default port to serve the app on
+const port = process.env.PORT || 5000
+app.listen(port)
+// Log a feedback that this is actually running
+console.log('Server started on port ' + port)
